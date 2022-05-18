05076a5db950917505bf658cac89aadd16a80a72
JoaoBreder <joao.g.breder@gmail.com>
2022-05-18 17:37:03 -0300

    Restaurant Order Bot

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
 const express = require('express');
+const bodyParser = require('body-parser');
+const bot = require('./bot');
+require('dotenv').config();
 
+
+// Server Port
+const PORT = 3000;
+
+// App inicialization and config
 const app = express();
-const port = 3000;
 
-app.post('/', (req, res) => {
-    client.messages
-        .create({
-            from: 'whatsapp:+14155238886',
-            body: 'Hello there!',
-            to: `whatsapp:${process.env.MY_PHONE_NUMBER}`
-        })
-        .then(message => console.log(message.sid));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+// Main route
+app.post('/', async (req, res) => {
+    await bot.processMessage(
+        req.body.Body,
+        req.body.From,
+        req.body.ProfileName
+    );
+
+    res.status(200).send('Ok');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });
