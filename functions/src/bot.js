46a95ad20666d332dc2f5038c0d6a7e279240b52
JoaoBreder <joao.g.breder@gmail.com>
2022-05-19 18:53:54 -0300

    Adjustments

--- functions/src/bot.js
+++ functions/src/bot.js
@@ -5,6 +5,8 @@ const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN
     lazyLoading: true
 });
 
+const orderNumbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
 let order;
 
 // Main function
@@ -31,11 +33,13 @@ O que você gostaria de pedir:
 7.  Refrigerante Lata (400 ml) - R$ 9.90
 8.  Suco Lata (400 ml) - R$ 9.90
 
-Para selecionar um item, digite 'adicionar' e o seu número. Se quiser ver o que já selecionou, digite 'carrinho'.
+Para selecionar um item basta digitar o seu número. Se quiser ver o que já selecionou, digite 'carrinho'.
 
 Caso tenha adicionado algum por engano, é só digitar 'remover' e o número do item no carrinho.
 
-Antes de finalizar o seu pedido, verifique se está tudo correto e então digite 'concluir pedido'.`;
+Antes de finalizar o seu pedido, verifique se está tudo correto e então digite 'concluir pedido'.
+
+Caso queira rever os comandos, digite 'ajuda'.`;
 
             sendMessage(senderId, message);
             order = new Order();
@@ -44,40 +48,90 @@ Antes de finalizar o seu pedido, verifique se está tudo correto e então digite
 
         const request = msg.toLowerCase();
 
-        if (request.match(/adicionar/)) {
-            const item = request.split('adicionar ')[1];
-            order.add(Number(item - 1));
+        switch (request) {
+            case 'ajuda':
+                sendMessage(
+                    senderId,
+                    `Comandos:
+- ajuda: Envia uma mensagem com os comandos do bot
+- menu: Menu do restaurante
+- carrinho: Mostra o seu carrinho de compras
+- remover {número do item no carrinho}: Remove o item especificado do carrinho
+- concluir pedido: Conclui o seu pedido e encaminha para o restaurante`
+                );
 
-            sendMessage(
-                senderId,
-                `Item ${item} adicionado ao carrinho.\n\nSe colocou o item errado por engano, digite 'carrinho' para ver o número do item e depois 'remover' para retirar o item especificado depois do comando.`
-            );
-        } else if (request.match(/carrinho/)) {
-            const message = order.listItens();
+                break;
 
-            sendMessage(
-                senderId,
-                message
-            );
-        } else if (request.match(/remover/)) {
-            const item = request.split('remover ')[1];
-            order.remove(Number(item) - 1);
 
-            sendMessage(
-                senderId,
-                `Item ${item} removido do carrinho.`
-            );
-        } else if (request.match(/concluir pedido/)) {
-            order = undefined;
-            sendMessage(
-                senderId,
-                'Seu pedido foi concluído e encaminhado para o restaurante. Obrigado e tenha uma ótima refeição.'
-            );
-        } else {
-            sendMessage(
-                senderId,
-                'Comando não reconhecido, por favor verifique se digitou corretamente e tente mais uma vez.'
-            );
+            case 'menu':
+                sendMessage(
+                    senderId,
+                    `- Refeições
+1.  Contra filé com fritas - R$20.00
+2.  Contra filé a parmegiana - R$20.00
+3.  Bife amilanesa - R$18.00
+
+- Lanches
+4.  Hambúrguer - R$22.00
+5.  Porção Grande de Batata Frita - R$12.00
+
+- Sobremesa
+6.  Sundae de Chocolate - R$ 5.60
+
+- Bebidas
+7.  Refrigerante Lata (400 ml) - R$ 9.90
+8.  Suco Lata (400 ml) - R$ 9.90`
+                );
+
+                break;
+
+            case 'carrinho':
+                const message = order.listItens();
+
+                sendMessage(
+                    senderId,
+                    message
+                );
+
+                break;
+
+            case 'concluir pedido':
+                order = undefined;
+                sendMessage(
+                    senderId,
+                    'Seu pedido foi concluído e encaminhado para o restaurante. Obrigado e tenha uma ótima refeição.'
+                );
+
+                break;
+
+
+            default:
+                if (orderNumbers.find((item) => item === '3')) {
+                    order.add(Number(request) - 1);
+                    sendMessage(
+                        senderId,
+                        `Item ${request} adicionado ao carrinho.\n\nSe colocou o item errado por engano, digite 'carrinho' para ver o número do item e depois 'remover {número do item}' para retirar o item especificado depois do comando.`
+                    );
+
+                    return;
+                }
+
+                if (request.match(/remover/)) {
+                    const item = request.split('remover ')[1];
+                    order.remove(Number(item) - 1);
+
+                    sendMessage(
+                        senderId,
+                        `Item ${item} removido do carrinho.`
+                    );
+                }
+
+                sendMessage(
+                    senderId,
+                    'Comando não reconhecido, por favor verifique se digitou corretamente e tente mais uma vez.'
+                );
+
+                break;
         }
     } catch (error) {
         console.log(`Error => ${error}`);
